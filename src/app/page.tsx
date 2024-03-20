6c9677373ab34607c9d9b669ac7f5f3e54a1d942
Gneissy <ahmetserteserr@gmail.com>
2024-03-20 09:18:54 +0300

    Update layout.tsx and page Create Header and Header-Auth Delete test.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,6 @@
-import { Button } from "@nextui-org/react";
-import { signIn, signOut } from "@/actions";
-import { auth } from "@/auth";
-import { Test } from "@/components/test";
 
 export default async function Home() {
-    const session = await auth();
-
     return (
-        <>
-            <Button color = "danger" isLoading>
-                Next UI Test
-            </Button>
-
-            {session?.user ? <p> Çardasın {JSON.stringify(session?.user)} </p> : <p> Çarda değilsin </p>}
-
-            <form action = {signIn}>
-                <Button type = "submit"> Giriş Yap </Button>
-            </form>
-            <form action = {signOut}>
-                <Button type = "submit"> Çıkış Yap </Button>
-            </form>
-
-            <p>Bu da client component:</p>
-            <Test />
-        </>
+        <> Home Page </>
     );
 }
