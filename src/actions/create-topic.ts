0fc6f9d70da85eb39145903fae323152b1aee045
Gneissy <ahmetserteserr@gmail.com>
2024-03-21 09:49:09 +0300

    Create TopicCreateForm component to be able to create topic popup form validation based on fields is added via Zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,43 @@
 "use server";
 
-export async function createTopic(){
+import { db } from "@/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+interface CreateTopicFormState{
+    errors: {
+        slug?: string[],
+        description?: string[],
+    }
+}
+
+const createTopicSchema = z.object({
+    slug: z.string().min(3).regex(/^[a-z-]+$/, {message: "Only letters, minimum 3, dash allowed."}),
+    description: z.string().min(10),
+});
+
+//? FormState is automatically added as first argument
+export async function createTopic(
+    formState: CreateTopicFormState, 
+    formData: FormData)
+    : Promise<CreateTopicFormState>{
+    const result = createTopicSchema.safeParse({
+        slug: formData.get("slug"),
+        description: formData.get("description")
+    });
+
+    if (result.success) {}
+    else {
+        return {
+            errors: result.error.flatten().fieldErrors
+        };
+    }
+
+    return {
+        errors: {}
+    }
+
 
     //TODO Revalidate homepage
 }
---
+++ src/components/topics/TopicCreateForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { createTopic } from "@/actions";
+import { Button, Popover, PopoverContent, PopoverTrigger, Input } from "@nextui-org/react";
+import { useFormState } from "react-dom";
+
+export default function TopicCreateForm(){
+
+    const [formState, action] = useFormState(createTopic, {
+        errors: {}
+    });
+
+    return (
+        <>
+            <Popover placement="left-start">
+                <PopoverTrigger>
+                    <Button color="primary"> Create Topic </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <div className="p-4">
+                        <form action = { action } className = "flex flex-col gap-4">
+                            <Input 
+                                name = "slug" 
+                                label = "Name" 
+                                labelPlacement = "outside" 
+                                placeholder= "Javascript" 
+                                isInvalid = {!!formState.errors.slug}
+                                errorMessage = {formState.errors.slug?.join(", ")}
+                            />
+                            <Input 
+                                name = "description" 
+                                label = "Description" 
+                                labelPlacement = "outside" 
+                                placeholder= "A programming language" 
+                                isInvalid = {!!formState.errors.description}
+                                errorMessage = {formState.errors.description?.join(", ")}
+                            />
+                            <Button type = "submit" color="secondary"> Create </Button>
+                        </form>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        </>
+    );
+}
